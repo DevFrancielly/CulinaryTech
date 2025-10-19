14f76d9efe12bf7ccc83154d3d29a3398b3f64e9
Francielly Silva <dev.francielly@gmail.com>
2025-10-18 21:11:54 -0300

    Criacao do header

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,15 @@
+import "./styles.css";
 import "../../components/header/header.jsx";
 import Header from "../../components/header/header";
+import imgBanner from "../../assets/bannerPrincipal.png";
 
 function Home() {
   return (
     <div>
       <Header />
+      <div className="banner-container">
+        <img src={imgBanner} alt="Imagem do banner principal" className="banner" />
+      </div>
     </div>
   );
 }
